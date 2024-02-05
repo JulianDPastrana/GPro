3b17b9b6d1c3ae487feded39a629d8f25623718d
JulianDPastrana <32947962+JulianDPastrana@users.noreply.github.com>
2024-02-05 11:44:03 -0500

    Add files via upload

--- some_test.py
+++ some_test.py
@@ -39,7 +39,7 @@ def initialize_data(N):
     f2 = np.cos(X)
 
     # Ensure positive scale values
-    alpha = np.exp(f1)
+    alpha = f1
     beta = np.exp(f2)
 
     Y = pred_distribution(alpha, beta).sample().numpy()
@@ -78,8 +78,12 @@ def train_model(model, data, epochs=100, log_freq=20):
     """
     Trains the model for a specified number of epochs.
     """
+    X, Y = data
     loss_fn = model.training_loss_closure(data)
-    natgrad_opt = gpf.optimizers.NaturalGradient(gamma=0.1)
+    gpf.utilities.set_trainable(model.q_mu, False)
+    gpf.utilities.set_trainable(model.q_sqrt, False)
+    natgrad_opt = gpf.optimizers.NaturalGradient(gamma=0.001)
+    # adam_var = tf.optimizers.Adam(0.001)
     adam_opt = tf.optimizers.Adam(0.01)
 
     variational_vars = [(model.q_mu, model.q_sqrt)]
@@ -90,10 +94,16 @@ def train_model(model, data, epochs=100, log_freq=20):
         natgrad_opt.minimize(loss_fn, variational_vars)
         adam_opt.minimize(loss_fn, adam_vars)
 
+    loss = []
     for epoch in range(1, epochs + 1):
         optimization_step()
         if epoch % log_freq == 0:
             print(f"Epoch {epoch} - Loss: {loss_fn().numpy() : .4f}")
+            Ymean, _ = model.predict_y(X)
+            Xrange = np.arange(X.shape[0])
+            plt.plot(Xrange, Ymean, color="k")
+            plt.scatter(Xrange, Y)
+            plt.show()
 
 
 def plot_results(model, X, Y):
@@ -101,18 +111,19 @@ def plot_results(model, X, Y):
     Plots the results of the model.
     """
     Xrange = np.arange(X.shape[0])[:, None]
-    num_samples = 5
+    num_samples = 100
 
-    num_y_pixels = 50
+    num_y_pixels = 100
     #Want the top left pixel to be evaluated at 1
     
     xlim_min, xlim_max = Xrange[:, 0].min(), Xrange[:, 0].max()
     ylim_min, ylim_max = Y[:, 0].min(), Y[:, 0].max()
 
     Fsamples = model.predict_f_samples(X, num_samples)
+    print(Fsamples.shape)
     alpha = np.exp(Fsamples[..., 0].numpy().squeeze())
     beta = np.exp(Fsamples[..., 1].numpy().squeeze())
-    line = np.linspace(np.ceil(ylim_max), 0.0, num_y_pixels)
+    line = np.linspace(np.floor(ylim_min), np.ceil(ylim_max), num_y_pixels)
     res = np.zeros((X.shape[0], num_y_pixels))
     for j in range(X.shape[0]):
         sf = alpha[:, j]  # Pick out the jth point along X axis
@@ -130,37 +141,46 @@ def plot_results(model, X, Y):
 
 
     norm = matplotlib.colors.Normalize(vmax=vmax, vmin=vmin)
-    x, y = np.mgrid[xlim_min:xlim_max:complex(res.shape[0]),
-                    ylim_max:ylim_min:complex(res.shape[1])]
-
-
-    fig3d = plt.figure(figsize=(15,8))
-    ax = fig3d.add_subplot(111, projection='3d')
-    ax.plot_surface(x,y,res,rstride=1, cmap=plt.cm.YlOrRd, cstride=1, lw=0.05, alpha=1, edgecolor='b', norm=norm)
+    x, y = np.meshgrid(Xrange, line)
+    print(x.shape, y.shape, res.shape)
+    fig, ax = plt.subplots(subplot_kw={"projection": "3d"}, figsize=(15, 8))
+    cs = ax.plot_surface(x, y, res.T, cmap=cm.viridis, norm=norm)
+    fig.colorbar(cs, ax=ax)
     ax.set_zlabel('PDF')
     ax.set_ylabel('Y')
     ax.set_xlabel('X')
     plt.show()
 
     fig, ax = plt.subplots(figsize=(15, 8))
-    cs = ax.contourf(x, y, res)
+    cs = ax.contourf(x, y, res.T, levels=150)
     fig.colorbar(cs, ax=ax)
-    ax.contour(cs, colors='k')
+    # ax.contour(cs, colors='k')
     ax.scatter(Xrange, Y, color="gray", alpha=0.5)
     Ymean, Yvar = model.predict_y(X)
-    ax.plot(Xrange, Ymean.numpy(), color="blue", lw=5)
+    ax.plot(Xrange, Ymean.numpy(), color="blue", lw=3, marker="x")
     plt.show()
 
 
 # Main execution
-N = 4001
+# N = 2001
 pred_distribution = tfp.distributions.Gamma
 # X, Y = initialize_data(N)
 train_data, test_data, column_names = streamflow_dataset(input_width=1)
 x_test, y_test = test_data
 x_train, y_train = train_data
-task = 0
+task = 2
 X, Y = x_train[6000:, task][:, None], y_train[6000:, task][:, None]
+print(X[690], X[691])
 model = build_model(X, Y)
 # train_model(model, (X, Y), epochs=100)
+
+
+def mean_squared_error(y, y_pred):
+        return np.mean((y - y_pred) ** 2)
+
+y_pred = model.predict_y(x_test[:, task][:, None])
+print(mean_squared_error(y_test[:, task][:, None], y_pred))
+X, Y = x_test[:, task][:, None], y_test[:, task][:, None]
+plt.scatter(X, Y)
+plt.show()
 plot_results(model, X, Y)
