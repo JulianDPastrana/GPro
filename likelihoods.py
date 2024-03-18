54097c41a8085a20ca9f327c649bcc78d3aa81a0
JulianDPastrana <j.pastrana@utp.edu.co>
2024-03-18 17:41:18 -0500

    Fix some issues

--- likelihoods.py
+++ likelihoods.py
@@ -6,30 +6,30 @@ import tensorflow as tf
 import numpy as np
 from gpflow.quadrature import GaussianQuadrature, NDiagGHQuadrature
 from gpflow.quadrature.gauss_hermite import gh_points_and_weights
+import tensorflow.experimental.numpy as tnp
 
 
 class LogNormalLikelihood(Likelihood):
     def __init__(self, input_dim, latent_dim, observation_dim) -> None:
         super().__init__(input_dim=1, latent_dim=2, observation_dim=1)
+        self.eps = tf.cast(1e-6, tf.float64)
 
     def _log_prob(self, X, F, Y) -> Tensor:
-        eps = 1e-6
-        mu = F[..., :1]
-        sigma = tf.math.exp(F[..., 1:])
-        norm_term = -tf.math.log(Y+eps)-tf.math.log(2*np.pi)-0.5*tf.math.log(sigma)
-        unorm_term = -0.5 * tf.square(tf.math.log(Y+eps) - mu) / sigma
-        return tf.reduce_sum(norm_term + unorm_term, axis=-1)
+        # mu = F[..., :1]
+        # sigma = tnp.exp(F[..., 1:])
+        # norm_term = -tnp.log(Y+self.eps)-0.5*tnp.log(2*np.pi)-0.5*tnp.log(sigma)
+        # unorm_term = -0.5 * tnp.square(tnp.log(Y+self.eps) - mu) / sigma
+        # return tf.reduce_sum(norm_term + unorm_term, axis=-1)
+        raise NotImplementedError
     
     def _variational_expectations(self, X, Fmu, Fvar, Y) -> Tensor:
-        eps = np.float64(1e-6)
         muf = Fmu[..., :1]
         mug = Fmu[..., 1:]
         sigmaf = Fvar[..., :1]
         sigmag = Fvar[..., 1:]
-        A = -tf.math.log(Y+eps)-tf.math.log(2*np.float64(np.pi))
-        B = -0.5*mug - 0.5*tf.math.log(Y+eps)**2 * tf.math.exp(-mug + sigmag/2) + tf.math.log(Y+eps)*muf*tf.math.exp(-mug + sigmag/2)
-        C = -0.5 * (sigmaf**2 + muf**2) * tf.math.exp(-mug + sigmag/2)
-        return tf.reduce_sum(A+B+C, axis=-1)
+        term1 = tnp.log(2*tnp.pi) + 2*tnp.log(Y+self.eps) + mug
+        term2 = tnp.exp(-mug + sigmag/2) * (tnp.square(tf.math.log(Y+self.eps)) - 2*tnp.log(Y+self.eps)*muf + sigmaf + tnp.square(muf))
+        return -0.5 * tf.reduce_sum(term1+term2, axis=-1)
     
     def _predict_mean_and_var(self, X, Fmu, Fvar):
         mean_f = Fmu[..., :1]
